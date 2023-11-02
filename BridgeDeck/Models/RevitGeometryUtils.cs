76f4d13f342c9094f42fe11f72fbd9103bd273b4
Денис Щербатюк <denvol100@gmail.com>
2023-11-02 15:09:15 +0300

    Fix bug when plane intersect more then one line

--- BridgeDeck/Models/RevitGeometryUtils.cs
+++ BridgeDeck/Models/RevitGeometryUtils.cs
@@ -50,6 +50,8 @@ namespace BridgeDeck.Models
         // Получение линии из списка, которая пересекается с плоскостью
         public static Line GetIntersectCurve(IEnumerable<Line> lines, Plane plane)
         {
+            var intersectionLines = new List<Line>();
+
             XYZ originPlane = plane.Origin;
             XYZ directionLine = plane.XVec;
 
@@ -69,8 +71,17 @@ namespace BridgeDeck.Models
                 var compResult = lineByPlane.Intersect(baseLine, out result);
                 if (compResult == SetComparisonResult.Overlap)
                 {
-                    return line;
+                    intersectionLines.Add(line);
                 }
+            }
+
+            if (intersectionLines.Count == 1)
+            {
+                return intersectionLines.First();
+            }
+            else if (intersectionLines.Count > 1)
+            {
+                return intersectionLines.OrderBy(l => l.Evaluate(0.5, true).DistanceTo(plane.Origin)).First();
             }
 
             return null;
